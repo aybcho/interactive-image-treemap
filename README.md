77397ed0df8243ca423a60786a560ed002c3d82c
yalexc <yalexcho@gmail.com>
2020-02-09 15:11:53 -0500

    Initial load of files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # interactive-image-treemap
-Interactive image treemap written in D3.js
+Interactive image treemap written in D3.js.
+
+This implementation was built based on Mike Bostocks's example of a D3 treemap
+https://bl.ocks.org/mbostock/6bbb0a7ff7686b124d80
+and
+https://bl.ocks.org/shimizu/79409cca5bcc57c32ddae0a5f0a1a564.
+
+Please feel free to use it at will with attribution of source.
---
+++ js/data.js
@@ -0,0 +1,82 @@
+let treemapData = [
+  {
+    id:'images',
+    value: '',
+    desc:'',
+    width: '',
+    height:'',
+    dir: ''
+  },
+  {
+    id:'images.Fish',
+    value: '3000',
+    desc:'Pictures of Fish',
+    width: '1920',
+    height:'1080',
+    dir: './images/fish.jpg'
+  },
+  {
+    id:'images.Flowers',
+    value: '3000',
+    desc:'Pictures of Flowers',
+    width: '1920',
+    height:'1080',
+    dir: './images/flowers.jpg'
+  },
+  {
+    id:'images.GoldenGateBridge',
+    value: '3000',
+    desc:'Pictures of the Golden Gate Bridge',
+    width: '1920',
+    height:'1080',
+    dir: './images/goldengatebridge.jpg'
+  },
+  {
+    id:'images.GreatWall',
+    value: '3000',
+    desc:'Pictures of the Great Wall',
+    width: '1920',
+    height:'1080',
+    dir: './images/greatwall.jpg'
+  },
+  {
+    id:'images.Mountains',
+    value: '3000',
+    desc:'Pictures of mountains',
+    width: '1920',
+    height:'1080',
+    dir: './images/mountains.jpg'
+  },
+  {
+    id:'images.NewYork',
+    value: '3000',
+    desc:'Pictures of New York',
+    width: '1920',
+    height:'1080',
+    dir: './images/newyork.jpg'
+  },
+  {
+    id:'images.Road',
+    value: '3000',
+    desc:'Pictures of some road',
+    width: '1920',
+    height:'1080',
+    dir: './images/road.jpg'
+  },
+  {
+    id:'images.Straws',
+    value: '3000',
+    desc:'Pictures of Colorful Straws',
+    width: '1920',
+    height:'1080',
+    dir: './images/straws.jpg'
+  },
+  {
+    id:'images.Trees',
+    value: '3000',
+    desc:'Pictures of Trees',
+    width: '1920',
+    height:'1080',
+    dir: './images/trees.jpg'
+  },
+];
